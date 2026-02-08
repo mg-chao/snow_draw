ec32a47ca1893e17c2dcdb93dec1d20dec743847
chao <chao@mgchao.top>
2026-02-09 01:04:57 +0800

    feat(core): register highlight element

---
+++ packages/snow_draw_core/lib/draw/elements/types/highlight/highlight_definition.dart
@@ -0,0 +1,18 @@
+﻿import 'package:flutter/material.dart';
+
+import '../../core/element_definition.dart';
+import '../../core/rect_creation_strategy.dart';
+import 'highlight_data.dart';
+import 'highlight_hit_tester.dart';
+import 'highlight_renderer.dart';
+
+final highlightDefinition = ElementDefinition<HighlightData>(
+  typeId: HighlightData.typeIdToken,
+  displayName: 'Highlight',
+  icon: Icons.highlight,
+  renderer: const HighlightRenderer(),
+  hitTester: const HighlightHitTester(),
+  createDefaultData: () => const HighlightData(),
+  fromJson: HighlightData.fromJson,
+  creationStrategy: const RectCreationStrategy(),
+);
---
+++ packages/snow_draw_core/test/highlight_registration_test.dart
@@ -0,0 +1,13 @@
+﻿import 'package:flutter_test/flutter_test.dart';
+import 'package:snow_draw_core/draw/elements/core/element_registry.dart';
+import 'package:snow_draw_core/draw/elements/registration.dart';
+import 'package:snow_draw_core/draw/elements/types/highlight/highlight_data.dart';
+
+void main() {
+  test('highlight is registered as a built-in element', () {
+    final registry = DefaultElementRegistry();
+    registerBuiltInElements(registry);
+
+    expect(registry.get(HighlightData.typeIdToken), isNotNull);
+  });
+}
