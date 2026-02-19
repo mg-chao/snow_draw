f37ed2a26a87ab418f93037e8999f2f922330eea
chao <chao@mgchao.top>
2026-02-19 21:52:00 +0800

    refactor: simplify edit operation registry interface

--- packages/snow_draw_core/lib/draw/edit/edit_operation_registry_interface.dart
+++ packages/snow_draw_core/lib/draw/edit/edit_operation_registry_interface.dart
@@ -1,9 +1,8 @@
 import '../types/edit_operation_id.dart';
 import 'core/edit_operation_base.dart';
 
-/// EditOperationRegistry abstraction for testability.
 abstract interface class EditOperationRegistry {
-  EditOperationBase? getOperation(EditOperationId operationId);
+  EditOperationBase? getOperation(EditOperationId id);
   Iterable<EditOperationBase> get allOperations;
   Iterable<EditOperationId> get allOperationIds;
 }
