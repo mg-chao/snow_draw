5b7d093e1828cd64af2edd378bcae397fb7a25df
chao <chao@mgchao.top>
2026-02-20 01:08:00 +0800

    Refactor resize geometry mode mapping

--- packages/snow_draw_core/lib/draw/edit/resize/bounds/resize_geometry.dart
+++ packages/snow_draw_core/lib/draw/edit/resize/bounds/resize_geometry.dart
@@ -6,23 +6,14 @@ import '../../../types/draw_rect.dart';
 ///
 /// The returned point is in the same coordinate space as [rect] (typically the
 /// selection overlay's un-rotated local frame).
-DrawPoint oppositeBoundPointLocal(DrawRect rect, ResizeMode mode) {
-  switch (mode) {
-    case ResizeMode.topLeft:
-      return DrawPoint(x: rect.maxX, y: rect.maxY);
-    case ResizeMode.topRight:
-      return DrawPoint(x: rect.minX, y: rect.maxY);
-    case ResizeMode.bottomRight:
-      return DrawPoint(x: rect.minX, y: rect.minY);
-    case ResizeMode.bottomLeft:
-      return DrawPoint(x: rect.maxX, y: rect.minY);
-    case ResizeMode.top:
-      return DrawPoint(x: rect.centerX, y: rect.maxY);
-    case ResizeMode.bottom:
-      return DrawPoint(x: rect.centerX, y: rect.minY);
-    case ResizeMode.left:
-      return DrawPoint(x: rect.maxX, y: rect.centerY);
-    case ResizeMode.right:
-      return DrawPoint(x: rect.minX, y: rect.centerY);
-  }
-}
+DrawPoint oppositeBoundPointLocal(DrawRect rect, ResizeMode mode) =>
+    switch (mode) {
+      ResizeMode.topLeft => DrawPoint(x: rect.maxX, y: rect.maxY),
+      ResizeMode.topRight => DrawPoint(x: rect.minX, y: rect.maxY),
+      ResizeMode.bottomRight => DrawPoint(x: rect.minX, y: rect.minY),
+      ResizeMode.bottomLeft => DrawPoint(x: rect.maxX, y: rect.minY),
+      ResizeMode.top => DrawPoint(x: rect.centerX, y: rect.maxY),
+      ResizeMode.bottom => DrawPoint(x: rect.centerX, y: rect.minY),
+      ResizeMode.left => DrawPoint(x: rect.maxX, y: rect.centerY),
+      ResizeMode.right => DrawPoint(x: rect.minX, y: rect.centerY),
+    };
