4dd319bed5dbe025a13a48a3a94ab1ec36b67dbe
chao <chao@mgchao.top>
2026-02-20 02:13:38 +0800

    Refine ElementRegistry interface documentation

--- packages/snow_draw_core/lib/draw/elements/core/element_registry_interface.dart
+++ packages/snow_draw_core/lib/draw/elements/core/element_registry_interface.dart
@@ -2,21 +2,16 @@ import 'element_data.dart';
 import 'element_definition.dart';
 import 'element_type_id.dart';
 
-/// ElementRegistry abstraction for testability.
+/// Registry contract for element definition lookup.
 abstract interface class ElementRegistry {
   ElementDefinition<T>? getDefinition<T extends ElementData>(
     ElementTypeId<T> typeId,
   );
 
-  /// Looks up a definition using the raw type value.
-  ///
-  /// This avoids allocating an [ElementTypeId] when the caller already has
-  /// the serialized type string.
   ElementDefinition<ElementData>? getDefinitionByValue(String typeValue);
 
   bool supports<T extends ElementData>(ElementTypeId<T> typeId);
 
-  /// Returns whether [typeValue] is currently registered.
   bool supportsTypeValue(String typeValue);
 
   Iterable<ElementTypeId<ElementData>> get registeredTypeIds;
