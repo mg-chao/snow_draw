28341501568a566b08071edbc910d669e31f271d
chao <chao@mgchao.top>
2026-02-20 07:09:08 +0800

    Make filter element definition const

--- packages/snow_draw_core/lib/draw/elements/types/filter/filter_definition.dart
+++ packages/snow_draw_core/lib/draw/elements/types/filter/filter_definition.dart
@@ -6,13 +6,13 @@ import 'filter_data.dart';
 import 'filter_hit_tester.dart';
 import 'filter_renderer.dart';
 
-final filterDefinition = ElementDefinition<FilterData>(
+const filterDefinition = ElementDefinition<FilterData>(
   typeId: FilterData.typeIdToken,
   displayName: 'Filter',
   icon: Icons.auto_fix_high,
-  renderer: const FilterRenderer(),
-  hitTester: const FilterHitTester(),
-  createDefaultData: () => const FilterData(),
+  renderer: FilterRenderer(),
+  hitTester: FilterHitTester(),
+  createDefaultData: FilterData.new,
   fromJson: FilterData.fromJson,
-  creationStrategy: const RectCreationStrategy(),
+  creationStrategy: RectCreationStrategy(),
 );
