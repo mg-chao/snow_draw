01cf81252fefc28c3a03cfd4f6f765efb6370eef
chao <chao@mgchao.top>
2026-02-20 22:39:44 +0800

    Simplify binding highlight visibility helper

--- packages/snow_draw_core/lib/draw/utils/binding_highlight_visibility.dart
+++ packages/snow_draw_core/lib/draw/utils/binding_highlight_visibility.dart
@@ -3,9 +3,4 @@ import '../elements/types/arrow/arrow_points.dart';
 String? resolveHoverBindingHighlightId({
   required String? hoveredBindingElementId,
   required ArrowPointHandle? hoveredArrowHandle,
-}) {
-  if (hoveredArrowHandle != null) {
-    return null;
-  }
-  return hoveredBindingElementId;
-}
+}) => hoveredArrowHandle == null ? hoveredBindingElementId : null;
